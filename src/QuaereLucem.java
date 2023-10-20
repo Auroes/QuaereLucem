e166a65713a08468e6bf70bd094b61b3186d4c52
Auroes <2510407339@qq.com>
2023-10-20 16:47:08 +0800

    💖UI💖

--- src/QuaereLucem.java
+++ src/QuaereLucem.java
@@ -14,7 +14,7 @@ public class QuaereLucem {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1920, 1080);
 
-        /** 物体面板 */
+        /** 1物体面板 */
         DefaultTableModel model1 = new DefaultTableModel();
         model1.addColumn("形状");
         model1.addColumn("材质");
@@ -24,13 +24,11 @@ public class QuaereLucem {
         model1.addColumn("宽度");
         model1.addColumn("高度");
         model1.addColumn("法向");
-
-        /** 视角面板 */
+        /** 2视角面板 */
         DefaultTableModel model2 = new DefaultTableModel();
         model2.addColumn("基点");
         model2.addColumn("法向");
-
-        /** 光源面板 */
+        /** 3光源面板 */
         DefaultTableModel model3 = new DefaultTableModel();
         model3.addColumn("形状");
         model3.addColumn("类型");
@@ -64,12 +62,12 @@ public class QuaereLucem {
         TableColumn materialColumn = table1.getColumnModel().getColumn(1);
         materialColumn.setCellEditor(new DefaultCellEditor(materialComboBox));
         /** 光源列表的可选项 */
-        JComboBox<String> shapeComboBox2 = new JComboBox<>(new String[]{"平板","球体"});
-        TableColumn shapeColumn2 = table2.getColumnModel().getColumn(0);
-        shapeColumn2.setCellEditor(new DefaultCellEditor(shapeComboBox2));
-        JComboBox<String> typeComboBox2 = new JComboBox<>(new String[]{"白光","红光","绿光","蓝光"});
-        TableColumn typeColumn2 = table2.getColumnModel().getColumn(1);
-        typeColumn2.setCellEditor(new DefaultCellEditor(typeComboBox2));
+        JComboBox<String> shapeComboBox3 = new JComboBox<>(new String[]{"平板","球体"});
+        TableColumn shapeColumn3 = table3.getColumnModel().getColumn(0);
+        shapeColumn3.setCellEditor(new DefaultCellEditor(shapeComboBox3));
+        JComboBox<String> typeComboBox3 = new JComboBox<>(new String[]{"白光","红光","绿光","蓝光"});
+        TableColumn typeColumn3 = table3.getColumnModel().getColumn(1);
+        typeColumn3.setCellEditor(new DefaultCellEditor(typeComboBox3));
 
         /** 初始化参数 */
         JComboBox<String> cComboBox = new JComboBox<>(new String[]{"0,0,0"});
@@ -91,6 +89,25 @@ public class QuaereLucem {
         JPanel buttonPanel3 = new JPanel();
         JButton addButton3 = new JButton("+");
         buttonPanel3.add(addButton3);
+        /** 输出面板的添加按钮 */
+        JPanel buttonPanel4 = new JPanel();
+        JButton addButton4 = new JButton("输出");
+        buttonPanel4.add(addButton4);
+
+        /** 解析度面板 */
+        JPanel resolutionPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JLabel resolutionLabel = new JLabel("解析度");
+        String[] resolutions = {"360", "720", "1080", "1440", "2160", "4320"};
+        JComboBox<String> resolutionComboBox = new JComboBox<>(resolutions);
+        resolutionPanel.add(resolutionLabel);
+        resolutionPanel.add(resolutionComboBox);
+
+        /** 基准三原色波长面板 */
+        JPanel wavelengthPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JLabel wavelengthLabel = new JLabel("基准三原色波长");
+        JTextField wavelengthTextField = new JTextField("R=564, G=534, B=420");
+        wavelengthPanel.add(wavelengthLabel);
+        wavelengthPanel.add(wavelengthTextField);
 
         /** 物体添加按钮的点击效果 */
         addButton1.addActionListener(new ActionListener() {
@@ -120,8 +137,8 @@ public class QuaereLucem {
         addButton3.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String shape  = (String) shapeComboBox2.getSelectedItem();
-                String type   = (String) typeComboBox2.getSelectedItem();
+                String shape  = (String) shapeComboBox3.getSelectedItem();
+                String type   = (String) typeComboBox3.getSelectedItem();
                 String center = (String) cComboBox.getSelectedItem();
                 String r      = (String) rComboBox.getSelectedItem();
                 String l      = (String) lComboBox.getSelectedItem();
@@ -130,15 +147,30 @@ public class QuaereLucem {
                 model3.addRow(new Object[]{shape, type, center, r, l, w,norm});
             }
         });
+        /** 输出按钮的点击效果 */
+        addButton4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
+            }
+        });
+
+        /** 场景面板上方标签 */
         JPanel topPanel1 = new JPanel(new BorderLayout());
         JLabel addLabel1 = new JLabel("配置场景物体");
-
+        /** 视角面板上方标签 */
         JPanel topPanel2 = new JPanel(new BorderLayout());
         JLabel addLabel2 = new JLabel("配置视角");
-
+        /** 光源面板上方标签 */
         JPanel topPanel3 = new JPanel(new BorderLayout());
         JLabel addLabel3 = new JLabel("配置光源");
+
+        /** 输出面板上方标签 */
+        JPanel topPanel4 = new JPanel(new BorderLayout());
+
+        JPanel labelPanel4 = new JPanel();
+        JLabel addLabel4 = new JLabel("输出配置");
+        labelPanel4.add(addLabel4);
 
         topPanel1.add(addLabel1, BorderLayout.WEST);
         topPanel1.add(buttonPanel1, BorderLayout.EAST);
@@ -146,35 +178,66 @@ public class QuaereLucem {
         topPanel2.add(buttonPanel2, BorderLayout.EAST);
         topPanel3.add(addLabel3, BorderLayout.WEST);
         topPanel3.add(buttonPanel3, BorderLayout.EAST);
+        topPanel4.add(labelPanel4, BorderLayout.WEST);
+        topPanel4.add(buttonPanel4, BorderLayout.EAST);
 
         /** 设置物体面板的位置 */
         JPanel Panel1 = new JPanel(new BorderLayout());
         Panel1.add(topPanel1, BorderLayout.NORTH);
         Panel1.add(scrollPane1, BorderLayout.CENTER);
-
         /** 设置视角面板的位置 */
         JPanel Panel2 = new JPanel(new BorderLayout());
         Panel2.add(topPanel2, BorderLayout.NORTH);
         Panel2.add(scrollPane2, BorderLayout.CENTER);
-
         /** 设置光源面板的位置 */
         JPanel Panel3 = new JPanel(new BorderLayout());
         Panel3.add(topPanel3, BorderLayout.NORTH);
         Panel3.add(scrollPane3, BorderLayout.CENTER);
 
+        /** 设置输出面板的位置 */
+        JPanel Panel4 = new JPanel(new BorderLayout());
+        Panel4.add(topPanel4, BorderLayout.NORTH);
+        //Panel4.setLayout(new BoxLayout(Panel4, BoxLayout.Y_AXIS));
+        JPanel setThings = new JPanel();
+        //setThings.setLayout(new BoxLayout(setThings, BoxLayout.Y_AXIS));
+        setThings.setLayout(new GridLayout(8,2,0,0));
+        setThings.add(resolutionPanel);
+        setThings.add(wavelengthPanel);
+        Panel4.add(setThings, BorderLayout.WEST);
+
         Panel1.setBorder(BorderFactory.createEmptyBorder(-5, 10, 10, 10));
         Panel2.setBorder(BorderFactory.createEmptyBorder(-5, 10, 10, 10));
         Panel3.setBorder(BorderFactory.createEmptyBorder(-5, 10, 10, 10));
-
-        //scrollPane1.setPreferredSize(new Dimension(500, 600));
-        //scrollPane2.setPreferredSize(new Dimension(500, 600));
+        Panel4.setBorder(BorderFactory.createEmptyBorder(-5, 10, 10, 10));
 
         frame.setLayout(new BorderLayout());
 
-        frame.add(Panel1, BorderLayout.WEST);
-        frame.add(Panel2, BorderLayout.EAST);
-        frame.add(Panel3, BorderLayout.SOUTH);
-1
+        frame.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        /** 设置面板的位置 */
+        c.gridx = 0;
+        c.gridy = 0;
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = 2.0;
+        c.weighty = 2.0;
+        frame.add(Panel1, c);
+        c.gridx = 0;
+        c.gridy = 2;
+        c.weightx = 2.0;
+        c.weighty = 2.0;
+        frame.add(Panel2, c);
+        c.gridx = 2;
+        c.gridy = 0;
+        c.weightx = 2.0;
+        c.weighty = 2.0;
+        frame.add(Panel3, c);
+        c.gridx = 2;
+        c.gridy = 2;
+        c.weightx = 2.0;
+        c.weighty = 2.0;
+        frame.add(Panel4, c);
+
         frame.setVisible(true);
     }
 }
